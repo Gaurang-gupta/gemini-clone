90cf53eb767b10f46ff02cfaf4c0f9cc0e430dd1
Gaurang-gupta <g12172001@gmail.com>
2024-09-06 19:41:54 +0530

    App Completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+const App = () => {
+  return (
+    <>
+    {/* 1:07:19 */}
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
